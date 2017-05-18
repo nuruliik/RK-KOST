c28fb2f29781abac23a6ea423c5ac693480ebd6e
ersaldyraisha <andi.ersaldy@gmail.com>
2017-05-19 02:49:31 +0700

    add image previews

--- fp-rk/resources/views/pages/index.blade.php
+++ fp-rk/resources/views/pages/index.blade.php
@@ -33,7 +33,7 @@
                                 <td>
                                     <ul class="unstyled soalOption">
                                         <li><a href="#soalSMAA" data-component="toggleme" data-target="#soalSMAA" style="font-size:1.2em; font-weight:bold">SMA (IPA) <span class="caret down"></span></a></li>
-                                        <ul id="soalSMAA" class="hide unstyled">
+                                        <ul id="soalSMAA" class="open unstyled">
                                             <li><a href="#!">B. Indonesia</a></li>
                                             <li><a href="#!">B. Inggris</a></li>
                                             <li><a href="#!">Matematika</a></li>
@@ -83,7 +83,7 @@
                             </tr>
                         </thead>
                         <tbody>
-                            @for($j=0;$j<5;$j++)
+                            @for($j=0;$j<8;$j++)
                             <tr>
                                 <td>Ujian Nasional 2016</td>
                                 <td>Ilmu Pengetahuan Alam</td>
@@ -105,30 +105,63 @@
 
 
 <div class="section">
-    <div class="row container gutters">
+    <div class="row container">
+        <div class="col col-12" style="margin-bottom:1.5rem">
+            <h2><i class="fa fa-question-circle" aria-hidden="true"></i> Kenapa Harus UNOJ?</h2>
+        </div>
+        <div class="col col-3">
+            <div style="padding-right:2rem; border-right:dashed 1px #d6d6d6; text-align:center">
+                <i class="fa fa-users fa-5x" aria-hidden="true" style="margin-bottom:1.5rem"></i>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sed purus eget felis aliquet finibus. Praesent congue eu justo at lobortis. </p>
+            </div>
+        </div>
+        <div class="col col-3">
+            <div style="padding:0 2rem; border-right:dashed 1px #d6d6d6; text-align:center">
+                <i class="fa fa-laptop fa-5x" aria-hidden="true" style="margin-bottom:1.5rem"></i>
+                <p>Nulla posuere rhoncus lacinia. Maecenas non risus neque. Quisque lorem nisi, viverra id odio quis, imperdiet aliquet. </p>
+            </div>
+        </div>
+        <div class="col col-3">
+            <div style="padding:0 2rem; border-right:dashed 1px #d6d6d6; text-align:center">
+                <i class="fa fa-tasks fa-5x" aria-hidden="true" style="margin-bottom:1.5rem"></i>
+                <p>Vestibulum vel quam nibh. Cras vehicula posuere tortor, at lobortis elit auctor ac. Vestibulum id libero eu ligula.</p>
+            </div>
+        </div>
+        <div class="col col-3">
+            <div style="padding-left:2rem; text-align:center">
+                <i class="fa fa-line-chart fa-5x" aria-hidden="true" style="margin-bottom:1.5rem"></i>
+                <p>Vestibulum vel quam nibh. Cras vehicula posuere tortor, at lobortis elit auctor ac. Vestibulum id libero eu ligula.</p>
+            </div>
+        </div>
+<!--
         <div class="col col-6">
             <h2><i class="fa fa-trophy" aria-hidden="true"></i> Peringkat</h2>
             <table>
                 <thead>
                     <tr>
-                        <td>Rank</td>
-                        <td>Username</td>
-                        <td>Nilai</td>
+                        <td>Peringkat</td>
+                        <td>Nama</td>
+                        <td>Sekolah</td>
+                        <td>Jenjang</td>
                         <td>Pengerjaan</td>
+                        <td>Nilai</td>
                     </tr>
                 </thead>
                 <tbody>
                     @for($i=1; $i<=5; $i++)
                     <tr>
                         <td>{{$i}}</td>
-                        <td>ersaldyraisha</td>
-                        <td>210</td>
+                        <td>Andi Ersaldy Raisha</td>
+                        <td>SMAN 78 Jakarta</td>
+                        <td>SMA (IPA)</td>
                         <td>12x</td>
+                        <td>210</td>
                     </tr>
                     @endfor
                 </tbody>
             </table>
         </div>
+-->
     </div>
 </div>
 
