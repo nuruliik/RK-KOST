4106844ed34a5c88a3b757b73e0cbce663278904
ersaldyraisha <andi.ersaldy@gmail.com>
2017-05-18 14:35:07 +0700

    add logo

--- fp-rk/resources/views/pages/index.blade.php
+++ fp-rk/resources/views/pages/index.blade.php
@@ -5,8 +5,8 @@
 @section('content')
 <div id="headline">
     <h1 style="margin-bottom:0; color:#fff">Jadilah Pelajar Cerdas</h1>
-    <p style="margin-bottom:2rem">Daftar UNOJ sekarang juga untuk kelancaran Ujian Nasionalmu Nanti.</p>
-    <button class="button secondary big"><b>Daftar dan Mulai Belajar</b></button>
+    <p style="margin-bottom:2rem">Daftar UNOJ sekarang juga untuk kelancaran Ujian Nasionalmu nanti.</p>
+    <button class="button secondary outline big"><b>Daftar dan Mulai Belajar</b></button>
 </div>
 @endsection
 
--- fp-rk/resources/views/parts/navbar.blade.php
+++ fp-rk/resources/views/parts/navbar.blade.php
@@ -1,7 +1,7 @@
 <div class="navbar container">
     <div class="row">
         <div class="col col-4">
-            <a href="#!" style=""><h4 style="margin-bottom:0; line-height:2.5rem">Ujian Nasional Online Judge</h4></a>
+            <a href="#!" style=""><h4 style="margin-bottom:0; line-height:2.5rem"><img src="img/logo64.png" style="height:2.5rem"></a>
         </div>
         <div class="col col-8">
             <a href="#!" class="float-right nav-item">Login</a>
