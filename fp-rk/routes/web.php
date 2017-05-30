ed3fa20c033d11a0fe7cba4bfa1e1eaa8b07e1dd
ersaldyraisha <andi.ersaldy@gmail.com>
2017-05-31 00:46:08 +0700

    add comment + status pengerjaan

--- fp-rk/routes/web.php
+++ fp-rk/routes/web.php
@@ -29,6 +29,10 @@ Route::get('/soal/create', function () {
     return view('pages.createSoal');
 });
 
+Route::get('/soal/comment', function () {
+    return view('pages.commentSoal');
+});
+
 Route::get('/soal/do', function () {
     return view('pages.doSoal');
 });
